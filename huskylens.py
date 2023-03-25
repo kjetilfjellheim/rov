c97b057d5aa45c77f1afae39bed4dbbe84e3feb4
Kjetil Fjellheim <kjetil@forgottendonkey.net>
2023-03-25 01:33:46 +0100

    Added changes for algorithms

--- huskylens.py
+++ huskylens.py
@@ -6,7 +6,19 @@ COMMAND_PRE = "55AA11"
 COMMAND_PING = "002c3c"
 COMMAND_FORGET = "003747"
 COMMAND_LEARNED = "002333"
-COMMAND_ALGORITM_COLOR_RECOGNITION = "022d0400"
+COMMAND_ALGORITHM_OBJECT_TRACKING = "022d0100"
+COMMAND_ALGORITHM_FACE_RECOGNITION = "022d0000"
+COMMAND_ALGORITHM_OBJECT_RECOGNITION = "022d0200"
+COMMAND_ALGORITHM_LINE_TRACKING = "022d0300"
+COMMAND_ALGORITHM_COLOR_RECOGNITION = "022d0400"
+COMMAND_ALGORITHM_TAG_RECOGNITION = "022d0500"
+COMMAND_ALGORITHM_OBJECT_CLASSIFICATION = "022d0600"
+COMMAND_ALGORITHM_QR_CODE_RECOGNTITION = "022d0700"
+COMMAND_ALGORITHM_BARCODE_RECOGNTITION = "022d0800"
+COMMAND_SCREESNSHOT = "003949"
+COMMAND_SAVE_PICTURE = "003040"
+COMMAND_SAVE_MODEL = "0232"
+COMMAND_ARROWS_BY_ID = "0228"
 COMMAND_LEARN = ""
 
 INITIAL_READ_SIZE = 5
@@ -16,34 +28,21 @@ ADDRESS_START = 4
 ADDRESS_STOP = 6
 DATA_LENGTH_START = 6
 DATA_LENGTH_STOP = 8
-DATA_LENGTH_BASE = 16
+BASE16 = 16
 COMMAND_START = 8
 COMMAND_STOP = 10
 DATA_START = 10
 
 class Response:
-
-    headers = None
-    address = None
-    command = None
-    data = None
-    checkSum = None
-
-    def __init__(self, responseData = None):
-        if (responseData == None):
-            self.success = False
-        else:
-            self.success = True
-            self.headers = responseData[HEADER_START:HEADER_STOP]
-            self.address = responseData[ADDRESS_START:ADDRESS_STOP]
-            self.command = responseData[COMMAND_START:COMMAND_STOP]
-            data_length = int(responseData[DATA_LENGTH_START:DATA_LENGTH_STOP], DATA_LENGTH_BASE)
-            if(data_length > 0):
-                self.data = responseData[DATA_START:DATA_START+data_length*2]
-            else:
-                self.data = []
-            self.checkSum = responseData[2*(6+data_length-1):2*(6+data_length-1)+2]
-
+    def __init__(self, success, headers = None, address = None, command = None, data = None, checksum = None, numberOfElements = None):
+        self.success = success
+        self.headers = headers
+        self.address = address
+        self.command = command
+        self.data = data
+        self.checksum = checksum
+        self.numberOfElements = numberOfElements
+        
     def pingSuccess(self):
         return "2e" == self.command
 
@@ -72,6 +71,32 @@ class Huskylens:
         return (len(initialResponse) == INITIAL_READ_SIZE)
     
     def command(self, command, id=None):
+        cmd = self.generateCommand(command, id)
+        self.writeCommand(cmd)
+        return self.handleResponse()
+
+    def handleResponse(self):
+        byteString = self.port.read(INITIAL_READ_SIZE)
+        if (self.isResponseReceived(byteString)):
+            byteString += self.port.read(int(byteString[3]) + 1)
+            responseData=byteString.hex()   
+            if (responseData == None):
+                return Response(False)
+            else:
+                headers = responseData[HEADER_START:HEADER_STOP]
+                address = responseData[ADDRESS_START:ADDRESS_STOP]
+                command = responseData[COMMAND_START:COMMAND_STOP]
+                data_length = int(responseData[DATA_LENGTH_START:DATA_LENGTH_STOP], BASE16)
+                data = []
+                numberOfElements = None
+                if(data_length > 0):
+                    data = responseData[DATA_START:DATA_START+data_length*2]  
+                    numberOfElements = int(data[2:4]+data[0:2], BASE16)                    
+                checksum = responseData[2*(6+data_length-1):2*(6+data_length-1)+2]                         
+                return Response(True, headers = headers, address = address, command = command, data = data, checksum = checksum, numberOfElements = numberOfElements)
+        return Response(False)
+
+    def generateCommand(self, command, id):
         cmd = COMMAND_PRE + command;
         if id != None:
             data = "{:04x}".format(id)
@@ -81,12 +106,7 @@ class Huskylens:
             dataLen = "{:02x}".format(len(data)//2)
             cmd = cmd + dataLen + "36" + data
         cmd += self.calculateChecksum(cmd)
-        self.writeCommand(cmd)
-        byteString = self.port.read(INITIAL_READ_SIZE)
-        if (self.isResponseReceived(byteString)):
-            byteString += self.port.read(int(byteString[3]) + 1)
-            return Response(responseData=byteString.hex())
-        return Response()
+        return cmd
     
     def writeCommand(self, cmd):
         self.port.flushInput()
@@ -100,5 +120,4 @@ class Huskylens:
             total += int(hexStr[i:i+2], 16)
         hexStr = hex(total)[-2:]
         return hexStr        
-
 
--- main.py
+++ main.py
@@ -1,12 +1,23 @@
-from huskylens import Huskylens, COMMAND_PING, COMMAND_FORGET, COMMAND_ALGORITM_COLOR_RECOGNITION, COMMAND_LEARNED, COMMAND_LEARN
+from huskylens import Huskylens, COMMAND_PING, COMMAND_FORGET, COMMAND_ALGORITHM_COLOR_RECOGNITION, COMMAND_LEARN, COMMAND_SCREESNSHOT, COMMAND_SAVE_PICTURE, COMMAND_ARROWS_BY_ID
 
 HUSKYLENS_SERIALNO = "AB0O5A7Z"
 
 huskylens = Huskylens(None, None, HUSKYLENS_SERIALNO)
 
 response = huskylens.command(command = COMMAND_PING)
-print(response)
+print(response.success)
 response = huskylens.command(command = COMMAND_FORGET)
-print(response)
-response = huskylens.command(command = COMMAND_LEARN, id=1)
-print(response)
+print(response.success)
+response = huskylens.command(command = COMMAND_ALGORITHM_COLOR_RECOGNITION)
+print(response.success)
+response = huskylens.command(command = COMM)
+print(response.success)
+response = huskylens.command(command = COMMAND_LEARN, id = 1)
+print(response.success)
+response = huskylens.command(command = COMMAND_SCREESNSHOT)
+print(response.success)
+response = huskylens.command(command = COMMAND_SAVE_PICTURE)
+print(response.success)
+response = huskylens.command(command = COMMAND_ARROWS_BY_ID, id = 1)
+print(response.success)
+print(response.numberOfElements)
