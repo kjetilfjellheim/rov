012a9006c747b6098c9fe3b425004bb4921e8b77
Kjetil Fjellheim <kjetil@forgottendonkey.net>
2023-04-05 00:00:55 +0200

    Rewritten motor control and changed logging for motor

--- lm298.py
+++ lm298.py
@@ -1,14 +1,21 @@
-from machine import Pin, PWM
+import logging
+import RPi.GPIO as gpio
 
 """
 Constants
 """
 LOGGER = "lm298"
+"""
+Logger setup
+"""
+logger = logging.getLogger("lm298")
 
 """
 Class definitions
 """
 class MotorControl:
+
+    PWM_FREQUENCY = 1000
 
     DIRECTION_LEFT = 1
     DIRECTION_RIGHT = -1
@@ -16,54 +23,64 @@ class MotorControl:
     DIRECTION_FORWARD = 1
     DIRECTION_BACK = -1
 
+    STOP_DUTY_CYCLE = 50
+
     NEGATE = -1
 
-    MAX_DUTY_CYCLE: int = 65535
-    STOP_DUTY_CYCLE: int = 32768
-    SPEED_CYCLE: int = int(STOP_DUTY_CYCLE / 100)
+    e1Pin = None
+    m1Pin = None
+    pwm1 = None
+    e2Pin = None
+    m2Pin = None
+    pwm2 = None
 
-    e1Pin: Pin
-    m1Pin: Pin
-    pwm1: PWM
-    e2Pin: Pin
-    m2Pin: Pin
-    pwm2: PWM
-
-    def __init__(self, e1Pin, m1Pin, e2Pin, m2Pin):
+    def __init__(self, e1Pin, m1Pin, m2Pin, e2Pin):
+        logger.info("Starting initalizing LM298")
         self.e1Pin = e1Pin
         self.m1Pin = m1Pin
-        self.e2Pin = e2Pin
         self.m2Pin = m2Pin
-        self.pwm1 = PWM(m1Pin)
-        self.pwm2 = PWM(m2Pin)
+        self.e2Pin = e2Pin
+        self.pwm1 = gpio.PWM(m1Pin, self.PWM_FREQUENCY)
+        self.pwm2 = gpio.PWM(m2Pin, self.PWM_FREQUENCY)
+        self.stop()
+        logger.info("Finished initalizing LM298")
 
     def enableMotors(self):
-        self.e2Pin.value(True)
-        self.e1Pin.value(True)
+        gpio.output(self.e1Pin, gpio.HIGH)
+        gpio.output(self.e2Pin, gpio.HIGH)
+        logger.info("Enabled motors LM298")
 
     def disableMotors(self):
-        self.e2Pin.value(False)
-        self.e1Pin.value(False)
+        gpio.output(self.e1Pin, gpio.LOW)
+        gpio.output(self.e2Pin, gpio.LOW)
+        logger.info("Disabled motors LM298")
         
     def setPwm(self, pwm, dutyCycle):
-        pwm.duty_u16(dutyCycle)
+        pwm.ChangeDutyCycle(dutyCycle)
+        if pwm == self.pwm1:
+            logger.info("PWM motor 1 dutycycle set {}", dutyCycle)
+        else:
+            logger.info("PWM motor 2 dutycycle set {}", dutyCycle)
 
     def rotateInPlace(self, speed, direction):
         self.enableMotors()
-        self.setPwm(self.pwm1, int(self.STOP_DUTY_CYCLE + (direction * speed * self.SPEED_CYCLE)))
-        self.setPwm(self.pwm2, int(self.STOP_DUTY_CYCLE + (direction * speed * self.SPEED_CYCLE)))
+        self.setPwm(self.pwm1, speed)
+        self.setPwm(self.pwm2, speed)
 
     def turnDifferential(self, speedLeft, speedRight):
+        logger.info("Motors turn differential")
         self.enableMotors()
-        self.setPwm(self.pwm1, int(self.STOP_DUTY_CYCLE + (self.DIRECTION_RIGHT * speedLeft * self.SPEED_CYCLE)))
-        self.setPwm(self.pwm2, int(self.STOP_DUTY_CYCLE + (self.DIRECTION_LEFT * speedRight * self.SPEED_CYCLE)))
+        self.setPwm(self.pwm1, speedLeft)
+        self.setPwm(self.pwm2, speedRight)
 
     def forward(self, speed, direction):
+        logger.info("Motors forward/reverse")
         self.enableMotors()
-        self.setPwm(self.pwm1, int(self.STOP_DUTY_CYCLE + (direction * speed * self.SPEED_CYCLE * self.NEGATE)))
-        self.setPwm(self.pwm2, int(self.STOP_DUTY_CYCLE + (direction * speed * self.SPEED_CYCLE)))
+        self.setPwm(self.pwm1, speed)
+        self.setPwm(self.pwm2, speed)
 
     def stop(self):
+        logger.info("Motors stop")
         self.disableMotors()
         self.setPwm(self.pwm1, self.STOP_DUTY_CYCLE)
         self.setPwm(self.pwm2, self.STOP_DUTY_CYCLE)
--- main.py
+++ main.py
@@ -2,14 +2,20 @@ import threading
 import logging
 import logging.config
 import yaml
+import time
+import RPi.GPIO as gpio
 from sen0386 import Sen0386
+from lm298 import MotorControl
 
 """
 Constants
 """
 LOGGER = "main"
 SEN0386_SERIALNO = "AB0O5A7Z"
-
+MOTORCONTROL_E1 = 6
+MOTORCONTROL_PWM1 = 18
+MOTORCONTROL_PWM2 = 19
+MOTORCONTROL_E2 = 20
 """
 Current sensor values
 """
@@ -21,15 +27,32 @@ Setup logging
 with open(file = 'logging.yaml', mode = 'r') as stream:
     config = yaml.load(stream = stream, Loader = yaml.FullLoader)
     logging.config.dictConfig(config)
-    
-logger = logging.getLogger(LOGGER)
 
+logger = logging.getLogger(LOGGER)
+logger.info("Logging setup")
 """
 Setting up modules
 """
 logger.info("Starting setup")
 sen0386 = Sen0386(serialno = SEN0386_SERIALNO)
 logger.info("Sen0386 setup")
+
+"""
+Setting up gpio
+"""
+logger.info("Starting setup gpio")
+gpio.setwarnings(False)		
+gpio.setmode(gpio.BOARD)
+gpio.setup(MOTORCONTROL_E1, gpio.OUT)
+gpio.setup(MOTORCONTROL_PWM1, gpio.OUT)
+gpio.setup(MOTORCONTROL_PWM2, gpio.OUT)
+gpio.setup(MOTORCONTROL_E2, gpio.OUT)
+logger.info("Finished setup gpio")
+
+"""
+Setting up motorcontrol
+"""
+motorControl = MotorControl(MOTORCONTROL_E1, MOTORCONTROL_PWM1, MOTORCONTROL_PWM2, MOTORCONTROL_E2)
 
 """
 Thread module setup
@@ -44,3 +67,12 @@ logger.info("Starting thread setup")
 sen0386Thread = threading.Thread(target = sen038Thread)
 sen0386Thread.start()
 logger.info("Completed Sen0386 setup")
+
+time.sleep(1)
+motorControl.disableMotors()
+time.sleep(1)
+motorControl.enableMotors()
+time.sleep(1)
+motorControl.forward(speed = 75, direction = MotorControl.DIRECTION_FORWARD)
+time.sleep(1)
+motorControl.stop()
